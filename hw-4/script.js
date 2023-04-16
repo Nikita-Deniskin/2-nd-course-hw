042837e95c7c0da0fceb72b21f767fa4d8251bc7
Nikita-Deniskin <ralermr@gmail.com>
2023-04-16 15:44:43 +0500

    done hw-4

---
+++ hw-4/script.js
@@ -0,0 +1,58 @@
+// let i = 0;
+// while (i < 2) {
+//     console.log("Привет");
+//     i++
+// }
+
+// let i = 0;
+// while (i < 5) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// let i = 7;
+// while (i <= 22) {
+// 	console.log(i);
+// 	i++;
+// }
+// let obj = {
+//     "Коля": 200,
+//     "Вася": 300,
+//     "Петя": 400
+// };
+// for(key in obj){
+//     console.log([key]+ ' - Зарплата '+ obj[key]+'$');
+// }
+
+// let n = 1000;
+// let num = 0;
+// while (n > 50) {
+//     num++;
+//     n /= 2;
+// }
+// console.log(n);
+// console.log(num);
+
+// let i = 1;
+
+// {for(i = 1; i <= 31; i+=7){
+//     let d = Number(prompt  ("Введите сегодняшнее число"));
+//     if (i = d)
+//     {alert (`Сегодня пятница ${i}-е, пора делать отчет`)}
+//     else (i != d)
+//     {alert ("Сегодня обычный день, отчет делать не нужно")}
+// }
+// }
+
+
+
+// let friday = Number(prompt  ("Введите дату первой пятницы месяца"));
+// if(friday <= 7 && friday >= 1) {
+//     for(i = friday; i <= 31; i+=7)
+//     {
+//         alert(`Сегодня пятница ${i}-е, пора делать отчет`)
+//     }
+// }
+// else {
+//     alert ("Повторите попытку")
+// }
